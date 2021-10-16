2c67301be9d784b42cc3165bec5979c0257dc45f
a0sa <bumpordna@gmail.com>
2021-10-16 23:49:34 +0900

    fix a typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const number = process.argv[2] || 0;
 let sum = 0;
 
-for( let i = 0 ; i < number ; i ++ ){
-    sum = sum + 1;
+for( let i = 1 ; i <= number ; i ++ ){
+    sum = sum + i;
 }
 console.log(sum);
